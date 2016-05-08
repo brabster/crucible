7cd4785935552ebbbfcb65186c018473c65b0673
Paul Brabban <paul.brabban@gmail.com>
2016-05-08 16:18:47 +0100

    enable travis deployment of snapshots

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
 (defproject crucible "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "AWS Cloudformation templates in Clojure"
+  :url "http://github.com/brabster/crucible"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies[[camel-snake-kebab "0.3.2"]
                 [org.clojure/clojure "1.8.0"]]
+  :repositories [["snapshots" {:url "https://clojars.org/repo"
+                              :username :env/clojars_username
+                              :password :env/clojars_password}]]
   :main ^:skip-aot crucible.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
