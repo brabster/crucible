6c39f17ec023ce674c51578d7e41f2c3a28250c7
Paul Brabban <paul.brabban@gmail.com>
2016-05-09 09:45:26 +0100

    Update project version for clojars
    To pick up new project description in search

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject crucible "0.1.0-SNAPSHOT"
+(defproject crucible "0.2.0-SNAPSHOT"
   :description "AWS Cloudformation templates in Clojure"
   :url "http://github.com/brabster/crucible"
   :license {:name "Eclipse Public License"
