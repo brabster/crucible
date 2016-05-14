7920e2778c8337bb3670cf46282c42445fa7e9f6
Paul Brabban <paul.brabban@gmail.com>
2016-05-14 13:16:45 +0100

    Wrong version

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies[[camel-snake-kebab "0.4.0"]
                 [org.clojure/clojure "1.8.0"]]
   :plugins [[lein-ancient "0.6.10"]
-            [lein-checkall "0.1.2"]]
+            [lein-checkall "0.1.1"]]
   :repositories [["snapshots" {:url "https://clojars.org/repo"
                               :username :env/clojars_username
                               :password :env/clojars_password}]
