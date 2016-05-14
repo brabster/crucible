7ed377fde5b5585d4e83dbe47722c73d02724f82
Paul Brabban <paul.brabban@gmail.com>
2016-05-14 13:05:35 +0100

    Update outdates deps

--- project.clj
+++ project.clj
@@ -3,8 +3,9 @@
   :url "http://github.com/brabster/crucible"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies[[camel-snake-kebab "0.3.2"]
+  :dependencies[[camel-snake-kebab "0.4.0"]
                 [org.clojure/clojure "1.8.0"]]
+  :plugins [[lein-ancient "0.6.10"]]
   :repositories [["snapshots" {:url "https://clojars.org/repo"
                               :username :env/clojars_username
                               :password :env/clojars_password}]
@@ -12,7 +13,6 @@
                             :username :env/clojars_username
                             :password :env/clojars_password
                             :sign-releases false}]]
-  :plugins [[lein-ancient "0.6.10"]]
   :main ^:skip-aot crucible.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
