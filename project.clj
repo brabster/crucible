cdfa2ac7e749eb92954847fdd52a3b652732ec1d
Paul Brabban <paul.brabban@gmail.com>
2016-05-14 13:01:43 +0100

    Try adding some plugins

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject crucible "0.4.0"
+(defproject crucible "0.4.1-SNAPSHOT"
   :description "AWS Cloudformation templates in Clojure"
   :url "http://github.com/brabster/crucible"
   :license {:name "Eclipse Public License"
@@ -12,6 +12,7 @@
                             :username :env/clojars_username
                             :password :env/clojars_password
                             :sign-releases false}]]
+  :plugins [[lein-ancient "0.6.10"]]
   :main ^:skip-aot crucible.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
