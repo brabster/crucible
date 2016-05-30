b068eb813a33fe0f60ff9eb643a23bc1f2dfa1d4
Paul Brabban <paul.brabban@gmail.com>
2016-05-30 14:27:31 +0100

    extend iam tests and fix typo

--- test/crucible/resources/aws/iam_test.clj
+++ test/crucible/resources/aws/iam_test.clj
@@ -4,11 +4,11 @@
             [crucible.resources.aws.iam :as iam]))
 
 (deftest test-example-iam-role
-  (testing ""
+  (testing "with singular actions and resources"
     (is (= (encode-resource
             {}
             (iam/role :assume-role-policy-document
-                      (iam/assume-role-policy-document [{:service "ec2.amazonaws.com"}])
+                      (iam/assume-role-policy-document {:service "ec2.amazonaws.com"})
                       :path "/"
                       :policies [(iam/policy "root"
                                              (iam/statement :actions "*"
@@ -20,9 +20,36 @@
              {"Version" "2012-10-17"
               "Statement"
               [{"Effect" "Allow",
-                "Principal" [{"Service" "ec2.amazonaws.com"}]
+                "Principal" {"Service" "ec2.amazonaws.com"}
                 "Action" ["sts:AssumeRole"]}]}
              "Policies"
              [{"Version" "2012-10-17"
                "Statement"
-               [{"Effect" "Allow" "Action" "*" "Resource" "*"}]}]}}))))
+               [{"Effect" "Allow" "Action" "*" "Resource" "*"}]}]}})))
+
+  (testing "with multiple actions and resources"
+    (is (= (encode-resource
+            {}
+            (iam/role :assume-role-policy-document
+                      (iam/assume-role-policy-document {:service "ec2.amazonaws.com"})
+                      :path "/"
+                      :policies [(iam/policy "root"
+                                             (iam/statement :actions ["s3:PutObject"
+                                                                      "s3:GetObject"]
+                                                            :resources ["arn:aws:s3:::mybucket/mykey"]))]))
+           {"Type" "AWS::IAM::Role"
+            "Properties"
+            {"Path" "/"
+             "AssumeRolePolicyDocument"
+             {"Version" "2012-10-17"
+              "Statement"
+              [{"Effect" "Allow",
+                "Principal" {"Service" "ec2.amazonaws.com"}
+                "Action" ["sts:AssumeRole"]}]}
+             "Policies"
+             [{"Version" "2012-10-17"
+               "Statement"
+               [{"Effect" "Allow"
+                 "Action" ["s3:PutObject"
+                           "s3:GetObject"]
+                 "Resource" ["arn:aws:s3:::mybucket/mykey"]}]}]}}))))
