9f01d146d9d6f6a6d7ece7015779755a75df1bda
Paul Brabban <paul.brabban@gmail.com>
2016-06-12 17:54:43 +0100

    update ec2 resources and add a couple of sanity checks

--- src/crucible/aws/ec2.clj
+++ src/crucible/aws/ec2.clj
@@ -7,12 +7,11 @@
 
 (s/def ::cidr-block ::v/value)
 
-(defmethod r/properties-type ::vpc [_]
-  (s/keys :req [::cidr-block]
-          :opt [::enable-dns-support
-                ::enable-dns-hostnames
-                ::instance-tenancy
-                ::r/tags]))
+(s/def ::vpc (s/keys :req [::cidr-block]
+                     :opt [::enable-dns-support
+                           ::enable-dns-hostnames
+                           ::instance-tenancy
+                           ::r/tags]))
 
 (def vpc (r/resource-factory (ec2 "VPC") ::vpc))
 
@@ -20,19 +19,17 @@
 (s/def ::availability-zone ::v/value)
 (s/def ::map-public-ip-on-launch ::v/value)
 
-(defmethod r/properties-type ::subnet [_]
-  (s/keys :req [::vpc-id ::cidr-block]
-          :opt [::availability-zone
-                ::map-public-ip-on-launch
-                ::r/tags]))
+(s/def ::subnet (s/keys :req [::vpc-id ::cidr-block]
+                        :opt [::availability-zone
+                              ::map-public-ip-on-launch
+                              ::r/tags]))
 
 (def subnet (r/resource-factory (ec2 "Subnet") ::subnet))
 
 (s/def ::domain #{"vpc"})
 (s/def ::instance-id string?)
 
-(defmethod r/properties-type ::eip [_]
-  (s/keys :opt [::domain ::instance-id]))
+(s/def ::eip (s/keys :opt [::domain ::instance-id]))
 
 (def eip (r/resource-factory (ec2 "EIP") ::eip))
 
@@ -40,19 +37,22 @@
 (s/def ::eip string?)
 (s/def ::private-ip-address string?)
 
-(defmethod r/properties-type ::eip-association [_]
-  (s/keys :opt [::allocation-id ::eip ::instance-id ::network-interface-id ::private-ip-address]))
+(s/def ::eip-association (s/keys :opt [::allocation-id
+                                       ::eip
+                                       ::instance-id
+                                       ::network-interface-id
+                                       ::private-ip-address]))
 
 (def eip-association (r/resource-factory (ec2 "EIPAssociation") ::eip-association))
 
-(defmethod r/properties-type ::internet-gateway [_]
-  (s/keys :opt [::r/tags]))
+(s/def ::internet-gateway (s/keys :opt [::r/tags]))
+
+(s/def ::internet-gateway map?)
 
 (def internet-gateway (r/resource-factory (ec2 "InternetGateway") ::internet-gateway))
 
 (s/def ::vpc-id ::v/value)
 
-(defmethod r/properties-type ::vpc-gateway-attachment [_]
-  (s/keys :req [::vpc-id]
-          :opt [::internet-gateway-id
-                ::vpn-gateway-id]))
+(s/def ::vpc-gateway-attachment (s/keys :req [::vpc-id]
+                                        :opt [::internet-gateway-id
+                                              ::vpn-gateway-id]))
---
+++ test/crucible/ec2_test.clj
@@ -0,0 +1,13 @@
+(ns crucible.ec2-test
+  (:require  [clojure.test :refer :all]
+             [crucible.aws.ec2 :as ec2]
+             [crucible.resources :as r]
+             [clojure.spec :as s]))
+
+(deftest vpc-test
+  (testing "minimal spec"
+    (is (s/valid? ::r/resource (ec2/vpc {::ec2/cidr-block "1.2.3.4/24"})))))
+
+(deftest igw-test
+  (testing "minimal spec"
+    (is (s/valid? ::r/resource (ec2/internet-gateway {})))))
