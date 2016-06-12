7dcd6a99f9fa04b7c6184babed7ed499da8712c2
Paul Brabban <paul.brabban@gmail.com>
2016-06-12 16:04:16 +0100

    tidy values file, handle plain string case

--- src/crucible/values.clj
+++ src/crucible/values.clj
@@ -9,25 +9,41 @@
 (s/def ::values (s/+ ::value))
 (s/def ::index (s/and pos? integer?))
 
-(defmulti value-type ::type)
-(defmulti encode-value ::type)
-
-(defmethod encode-value :default [x] x)
+(defmulti value-type (fn [x] (or (::type x) (class x))))
 
 (s/def ::xref (s/keys :req [::type ::ref]
                       :opt [::att]))
 
 (s/def ::value (s/multi-spec value-type ::type))
 
+(defmethod value-type java.lang.String [_] string?)
+
 (defmethod value-type ::xref [_] ::xref)
+
+(s/def ::pseudo (s/keys :req [::type ::param]))
+
+(defmethod value-type ::pseudo [_] ::pseudo)
+
+(s/def ::join (s/keys :req [::type ::values]
+                      :opt [::delimiter]))
+
+(defmethod value-type ::join [_] ::join)
+
+(s/def ::select (s/keys :req [::type ::values ::index]))
+
+(defmethod value-type ::select [_] ::select)
+
+
+
+
+(defmulti encode-value ::type)
+
+(defmethod encode-value :default [x] x)
 
 (defmethod encode-value ::xref [{:keys [::ref ::att]}]
   (if att
     {"Fn::GetAtt" [(keys/->key ref) (keys/->key att)]}
     {"Ref" (keys/->key ref)}))
-
-(defmethod value-type ::pseudo [_]
-  (s/keys :req [::type ::param]))
 
 (defmethod keys/->key :notification-arns [_]
   "NotificationARNs")
@@ -35,18 +51,14 @@
 (defmethod encode-value ::pseudo [{:keys [::param]}]
   {"Ref" (str "AWS::" (-> param name keyword keys/->key))})
 
-(defmethod value-type ::join [_]
-  (s/keys :req [::type ::values]
-          :opt [::delimiter]))
-
 (defmethod encode-value ::join [{:keys [::delimiter ::values]}]
   {"Fn::Join" [(or delimiter "") (vec (map encode-value values))]})
 
-(defmethod value-type ::select [_]
-  (s/keys :req [::type ::values ::index]))
-
 (defmethod encode-value ::select [{:keys [::index ::values]}]
   {"Fn::Select" [(str index) (vec (map encode-value values))]})
+
+
+
 
 (defn xref
   ([ref]
