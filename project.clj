be75ee514800d4156ec9c2e052978ecc7a63d03a
Paul Brabban <paul.brabban@gmail.com>
2016-07-02 18:35:19 +0100

    update deps

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[camel-snake-kebab "0.4.0"]
-                 [cheshire "5.6.1"]
-                 [org.clojure/clojure "1.9.0-alpha5"]]
+                 [cheshire "5.6.3"]]
   :plugins [[lein-ancient "0.6.10"]
             [lein-kibit "0.1.2"]
             [jonase/eastwood "0.2.3"]
@@ -19,5 +18,6 @@
                               :sign-releases false}]] 
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
+             :provided {:dependencies [[org.clojure/clojure "1.9.0-alpha8"]]}
              :dev {:resource-paths ["test-resources"]
                    :dependencies [[org.clojure/test.check "0.9.0"]]}})
