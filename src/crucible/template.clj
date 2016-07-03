f2030eaf0b6bdadd76d92cecaa07c99b4a740d83
Paul Brabban <paul.brabban@gmail.com>
2016-07-03 18:43:51 +0100

    tidy and document

--- src/crucible/template.clj
+++ src/crucible/template.clj
@@ -1,4 +1,5 @@
 (ns crucible.template
+  "Commonly used template construction functions"
   (:require [clojure.spec :as s]
             [crucible.values :as v]
             [crucible.parameters :as p]
@@ -16,31 +17,33 @@
 (s/def ::template (s/cat :description ::description
                          :elements (s/nilable (s/map-of keyword? ::element))))
 
-(defn conform-or-throw [spec input]
-  (let [parsed (s/conform spec input)]
+(defn template "Make a template structure with the given description and elements"
+  [description & {:as elements}]
+  (let [input [description elements]
+        spec ::template
+        parsed (s/conform spec input)]
     (if (= parsed ::s/invalid)
       (throw (ex-info "Invalid input" (s/explain-data spec input)))
       parsed)))
 
-(defn template [description & {:as elements}]
-  (conform-or-throw ::template [description elements]))
-
-(defn parameter [& {:keys [type]
-                    :or {type ::p/string}
-                    :as options}] 
+(defn parameter "Make a template parameter element"
+  [& {:keys [type] :or {type ::p/string} :as options}]
   [:parameter (assoc options ::p/type type)])
 
-(defn resource [{:as options}]
+(defn resource "Make a template resource element"
+  [options]
   [:resource options])
 
-(defn output 
+(defn output "Make a template output with the value and an optional description"
   [value & [description]]
   [:output (-> description
                (when {::o/description description})
                (assoc ::o/value value))])
 
-(defn xref [& options]
+(defn xref "Cross-reference another template element"
+  [& options]
   (apply v/xref options))
 
-(defn encode [template]
+(defn encode "Encode a template into JSON for use by CloudFormation"
+  [template]
   (encoding/encode template))
