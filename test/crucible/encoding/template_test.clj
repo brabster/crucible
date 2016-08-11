3678df23dea187c02683fb74d3533ca63607bafd
Paul Brabban <paul@localhost.home>
2016-08-11 07:24:41 +0100

    sort out pseudo parameters

--- test/crucible/encoding/template_test.clj
+++ test/crucible/encoding/template_test.clj
@@ -2,10 +2,11 @@
   (:require [clojure.test :refer :all]
             [crucible.encoding :refer [encode]]
             [crucible.template :refer [template parameter output xref]]
-            [crucible.values :refer [join]]
+            [crucible.values :refer [join pseudo]]
             [crucible.parameters :as param]
             [crucible.aws.ec2 :as ec2]
-            [crucible.aws.dynamodb :as ddb]))
+            [crucible.aws.dynamodb :as ddb]
+            [crucible.values :as v]))
 
 (def vpc-crucible (ec2/vpc {::ec2/cidr-block "10.0.0.0/16"}))
 (def vpc-cf {"Type" "AWS::EC2::VPC"
@@ -103,6 +104,18 @@
                        :foo (parameter)
                        :my-resource (ec2/vpc {::ec2/cidr-block
                                               (join "-" [(xref :foo) "bar"])}))))))))
+
+(deftest template-value-pseudo-test
+  (testing "template with pseude-parameters"
+    (is (= {"AWSTemplateFormatVersion" "2010-09-09"
+            "Description"  "t"
+            "Resources" {"MyResource" {"Type" "AWS::EC2::VPC"
+                                       "Properties"
+                                       {"CidrBlock" {"Ref" "AWS::NotificationARNs"}}}}}
+           (cheshire.core/decode
+            (encode
+             (template "t"
+                       :my-resource (ec2/vpc {::ec2/cidr-block v/notification-arns}))))))))
 
 #_(deftest resource-reference-validation-test
     (testing "reference non-existent parameter from resource property throws"
