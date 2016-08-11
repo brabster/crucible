fc033003bba0eb724481f9e67095c305620441d8
Paul <paul.brabban@skybettingandgaming.com>
2016-08-11 14:10:48 +0100

    init kinesis

---
+++ src/crucible/aws/kinesis.clj
@@ -0,0 +1,16 @@
+(ns aws.kinesis
+  (require [clojure.spec :as s]
+           [crucible.values :as v]
+           [crucible.resources :as r]
+           [crucible.template :as t]))
+
+(s/def ::shard-count (v/spec-or-ref pos-int?))
+
+(s/def ::name (v/spec-or-ref string?))
+
+(s/def ::stream (s/keys :req [::shard-count]
+                        :opt [::name
+                              ::v/tags]))
+
+(def stream (r/resource-factory "AWS::Kinesis::Stream" ::stream))
+
