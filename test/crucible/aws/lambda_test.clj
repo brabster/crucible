b48fb4cba172a564b34d149abf5b53cb8257c34c
Paul Brabban <paul.brabban@gmail.com>
2016-08-19 19:23:14 +0100

    fixes #47

--- test/crucible/aws/lambda_test.clj
+++ test/crucible/aws/lambda_test.clj
@@ -4,7 +4,44 @@
              [crucible.resources :as res]
              [cheshire.core :as json]
              [clojure.spec :as s]
-             [crucible.core :as cru]))
+             [crucible.core :refer [xref encode template account-id parameter]]))
+
+(deftest function-test
+  (testing "encode"
+    (is (= {"AWSTemplateFormatVersion" "2010-09-09"
+            "Description" "minimal"
+            "Resources"
+            {"MyFunction"
+             {"Type" "AWS::Lambda::Function"
+              "Properties" {"FunctionName" {"Ref" "Foo"}
+                            "Timeout" 300
+                            "Handler" {"Ref" "Foo"}
+                            "Runtime" "java8"
+                            "MemorySize" 1024
+                            "Role" {"Ref" "Foo"}
+                            "Description" {"Ref" "Foo"}
+                            "VpcConfig"
+                            {"SecurityGroupIds" [{"Ref" "Foo"}], "SubnetIds" {"Ref" "Foo"}}
+                            "Code" {"S3Bucket" {"Ref" "Foo"}, "S3Key" {"Ref" "Foo"}}}}}
+            "Parameters" {"Foo" {"Type" "String"}}}
+           (json/decode
+            (encode
+             (template
+              "minimal"
+              :foo (parameter)
+              :my-function (lambda/function
+                            #::lambda{:handler (xref :foo)
+                                      :function-name (xref :foo)
+                                      :description (xref :foo)
+                                      :memory-size 1024
+                                      :timeout 300
+                                      :runtime "java8"
+                                      :role (xref :foo)
+                                      :code #::lambda{:s3-bucket (xref :foo)
+                                                      :s3-key (xref :foo)}
+                                      :vpc-config
+                                      #::lambda{:security-group-ids [(xref :foo)]
+                                                :subnet-ids (xref :foo)}}))))))))
 
 (deftest permission-test
   (testing "encode"
@@ -19,12 +56,12 @@
                             "SourceAccount" { "Ref" "AWS::AccountId" }}}}
             "Parameters" {"MyLambdaFunction" {"Type" "String"}}}
            (json/decode
-            (cru/encode
-             (cru/template
+            (encode
+             (template
               "minimal"
-              :my-lambda-function (cru/parameter)
+              :my-lambda-function (parameter)
               :my-permission
               (lambda/permission #::lambda{:action "lambda:InvokeFunction"
-                                           :function-name (cru/xref :my-lambda-function :arn)
+                                           :function-name (xref :my-lambda-function :arn)
                                            :principal "s3.amazonaws.com"
-                                           :source-account cru/account-id}))))))))
+                                           :source-account account-id}))))))))
