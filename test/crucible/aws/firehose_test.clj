e42bb4e6bea1dc713d9644f0357102c66051f4e4
garethdavies <gareth.davies@skybettingandgaming.com>
2016-08-24 15:48:20 +0100

    Rename tests and print spec exceptions that were being swallowed.

--- test/crucible/aws/firehose_test.clj
+++ test/crucible/aws/firehose_test.clj
@@ -4,32 +4,38 @@
             [cheshire.core :as json]
             [clojure.test :refer :all]))
 
-(deftest s3-bucket-policy-test
+(defmacro is-valid? [& resources]
+  `(try
+     ~@resources
+     (catch clojure.lang.ExceptionInfo e#
+       (is (nil? (ex-data e#)) "Resources are not valid"))))
+
+(deftest firehose-to-s3-test
   (testing "encode"
-    (is (= {"AWSTemplateFormatVersion" "2010-09-09",
-            "Description" "sample",
-            "Parameters" {"Foo" {"Type" "String"}},
-            "Resources" {"Firehose" {"Type" "AWS::KinesisFirehose::DeliveryStream",
-                                     "Properties" {"DeliveryStreamName" {"Ref" "Foo"},
-                                                   "S3DestinationConfiguration"
-                                                          {"BucketArn" {"Ref" "Foo"},
-                                                           "BufferingHints" {"IntervalInSeconds" 9,
-                                                           "SizeInMBs" {"Ref" "Foo"}},
-                                                           "CompressionFormat" "UNCOMPRESSED",
-                                                           "Prefix" {"Ref" "Foo"},
-                                                           "RoleArn" {"Ref" "Foo"}}}}}}
-           (json/decode
-            (encode
-             (template
-              "sample"
-              :foo (parameter)
-              :firehose
-              (fh/firehose
-               {::fh/delivery-stream-name (xref :foo)
-                ::fh/s3-destination-configuration
-                #::fh{:bucket-arn (xref :foo)
-                      :buffering-hints #::fh{:interval-in-seconds 9
-                                             :size-in-mbs (xref :foo)}
-                      :compression-format "UNCOMPRESSED"
-                      :prefix  (xref :foo)
-                      :role-arn  (xref :foo)}}))))))))
+    (let [t (is-valid?
+              (template
+                "sample"
+                :foo (parameter)
+                :firehose
+                (fh/firehose
+                  {::fh/delivery-stream-name (xref :foo)
+                   ::fh/s3-destination-configuration
+                                             #::fh {:bucket-arn      (xref :foo)
+                                                    :buffering-hints #::fh {:interval-in-seconds 9
+                                                                            :size-in-mbs         (xref :foo)}
+                                                                     :compression-format "UNCOMPRESSED"
+                                                                     :prefix (xref :foo)
+                                                                     :role-arn (xref :foo)}})))]
+      (is (= {"AWSTemplateFormatVersion" "2010-09-09",
+              "Description"              "sample",
+              "Parameters"     {"Foo" {"Type" "String"}},
+              "Resources"      {"Firehose" {"Type"    "AWS::KinesisFirehose::DeliveryStream",
+                                            "Properties" {"DeliveryStreamName" {"Ref" "Foo"},
+                                                                "S3DestinationConfiguration"
+                                                                     {"BucketArn"         {"Ref" "Foo"},
+                                                                      "BufferingHints"    {"IntervalInSeconds" 9,
+                                                                                           "SizeInMBs"  {"Ref" "Foo"}},
+                                                                      "CompressionFormat" "UNCOMPRESSED",
+                                                                      "Prefix"            {"Ref" "Foo"},
+                                                                      "RoleArn"           {"Ref" "Foo"}}}}}}
+            (json/decode (encode t)))))))
