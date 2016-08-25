6847f0966587c1408a6519211800db161d3dda8d
Paul Brabban <paul.brabban@gmail.com>
2016-08-25 06:30:19 +0100

    autocomplete added self ref to namespace
    broke cloverage

--- src/crucible/aws/ec2.clj
+++ src/crucible/aws/ec2.clj
@@ -1,8 +1,7 @@
 (ns crucible.aws.ec2
   "Resources in AWS::EC2::*"
   (:require [clojure.spec :as s]
-            [crucible.resources :refer [spec-or-ref defresource] :as res]
-            [crucible.aws.ec2 :as ec2]))
+            [crucible.resources :refer [spec-or-ref defresource] :as res]))
 
 (defn ec2 [suffix] (str "AWS::EC2::" suffix))
 
