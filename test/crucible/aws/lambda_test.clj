7a703d96e9cb4031fa6c44f0db3ae6e6a9209921
Paul Brabban <paul.brabban@gmail.com>
2016-08-26 07:12:51 +0100

    update tests to be clearer
    - use rewrite-element-data to remove noise of template
    - don't use namespaced maps anymore

--- test/crucible/aws/lambda_test.clj
+++ test/crucible/aws/lambda_test.clj
@@ -1,67 +1,46 @@
 (ns crucible.aws.lambda-test
   (:require  [clojure.test :refer :all]
-             [crucible.aws.lambda :as lambda]
+             [crucible.aws.lambda :as l]
              [crucible.resources :as res]
              [cheshire.core :as json]
              [clojure.spec :as s]
-             [crucible.core :refer [xref encode template account-id parameter]]))
+             [crucible.core :refer [xref account-id parameter]]
+             [crucible.encoding :refer [rewrite-element-data]]))
 
 (deftest function-test
   (testing "encode"
-    (is (= {"AWSTemplateFormatVersion" "2010-09-09"
-            "Description" "minimal"
-            "Resources"
-            {"MyFunction"
-             {"Type" "AWS::Lambda::Function"
-              "Properties" {"FunctionName" {"Ref" "Foo"}
-                            "Timeout" 300
-                            "Handler" {"Ref" "Foo"}
-                            "Runtime" "java8"
-                            "MemorySize" 1024
-                            "Role" {"Ref" "Foo"}
-                            "Description" {"Ref" "Foo"}
-                            "VpcConfig"
-                            {"SecurityGroupIds" [{"Ref" "Foo"}], "SubnetIds" {"Ref" "Foo"}}
-                            "Code" {"S3Bucket" {"Ref" "Foo"}, "S3Key" {"Ref" "Foo"}}}}}
-            "Parameters" {"Foo" {"Type" "String"}}}
-           (json/decode
-            (encode
-             (template
-              "minimal"
-              :foo (parameter)
-              :my-function (lambda/function
-                            #::lambda{:handler (xref :foo)
-                                      :function-name (xref :foo)
-                                      :description (xref :foo)
-                                      :memory-size 1024
-                                      :timeout 300
-                                      :runtime "java8"
-                                      :role (xref :foo)
-                                      :code #::lambda{:s3-bucket (xref :foo)
-                                                      :s3-key (xref :foo)}
-                                      :vpc-config
-                                      #::lambda{:security-group-ids [(xref :foo)]
-                                                :subnet-ids (xref :foo)}}))))))))
+    (is (= {"Type" "AWS::Lambda::Function"
+            "Properties" {"FunctionName" {"Ref" "Foo"}
+                          "Timeout" 300
+                          "Handler" {"Ref" "Foo"}
+                          "Runtime" "java8"
+                          "MemorySize" 1024
+                          "Role" {"Ref" "Foo"}
+                          "Description" {"Ref" "Foo"}
+                          "VpcConfig" {"SecurityGroupIds" [{"Ref" "Foo"}], "SubnetIds" {"Ref" "Foo"}}
+                          "Code" {"S3Bucket" {"Ref" "Foo"}, "S3Key" {"Ref" "Foo"}}}}
+           (rewrite-element-data
+            (lambda/function {::l/handler (xref :foo)
+                              ::l/function-name (xref :foo)
+                              ::l/description (xref :foo)
+                              ::l/memory-size 1024
+                              ::l/timeout 300
+                              ::l/runtime "java8"
+                              ::l/role (xref :foo)
+                              ::l/code {::l/s3-bucket (xref :foo)
+                                        ::l/s3-key (xref :foo)}
+                              ::l/vpc-config {::l/security-group-ids [(xref :foo)]
+                                              ::l/subnet-ids (xref :foo)}}))))))
 
 (deftest permission-test
   (testing "encode"
-    (is (= {"AWSTemplateFormatVersion" "2010-09-09"
-            "Description" "minimal"
-            "Resources"
-            {"MyPermission"
-             {"Type" "AWS::Lambda::Permission"
-              "Properties" {"FunctionName" { "Fn::GetAtt" ["MyLambdaFunction" "Arn"] }
-                            "Action" "lambda:InvokeFunction"
-                            "Principal" "s3.amazonaws.com"
-                            "SourceAccount" { "Ref" "AWS::AccountId" }}}}
-            "Parameters" {"MyLambdaFunction" {"Type" "String"}}}
-           (json/decode
-            (encode
-             (template
-              "minimal"
-              :my-lambda-function (parameter)
-              :my-permission
-              (lambda/permission #::lambda{:action "lambda:InvokeFunction"
-                                           :function-name (xref :my-lambda-function :arn)
-                                           :principal "s3.amazonaws.com"
-                                           :source-account account-id}))))))))
+    (is (= {"Type" "AWS::Lambda::Permission"
+            "Properties" {"FunctionName" { "Fn::GetAtt" ["MyLambdaFunction" "Arn"] }
+                          "Action" "lambda:InvokeFunction"
+                          "Principal" "s3.amazonaws.com"
+                          "SourceAccount" { "Ref" "AWS::AccountId" }}}
+           (rewrite-element-data
+            (lambda/permission {::l/action "lambda:InvokeFunction"
+                                ::l/function-name (xref :my-lambda-function :arn)
+                                ::l/principal "s3.amazonaws.com"
+                                ::l/source-account account-id}))))))
--- test/crucible/aws/s3_test.clj
+++ test/crucible/aws/s3_test.clj
@@ -1,5 +1,6 @@
 (ns crucible.aws.s3-test
-  (:require [crucible.core :refer [encode template parameter xref]]
+  (:require [crucible.core :refer [parameter xref]]
+            [crucible.encoding :refer [rewrite-element-data]]
             [crucible.aws.s3 :as s3]
             [crucible.aws.iam :as iam]
             [cheshire.core :as json]
@@ -8,60 +9,46 @@
 
 (deftest s3-bucket-policy-test
   (testing "encode"
-    (is (= {"AWSTemplateFormatVersion" "2010-09-09",
-            "Description" "sample",
-            "Resources"
-            {"BucketPolicy"
-             {"Type" "AWS::S3::BucketPolicy",
-              "Properties"
-              {"Bucket" {"Ref" "Foo"},
-               "PolicyDocument"
-               {"Statement"
-                [{"Action" ["s3:GetObject"],
-                  "Effect" "Allow",
-                  "Principal" {"Service" "lambda.amazonaws.com"},
-                  "Condition" {"StringEquals" {"aws:SourceArn" {"Ref" "Foo"}}},
-                  "Resource" {"Ref" "Foo"}}]}}}},
-            "Parameters" {"Foo" {"Type" "String"}}}
-           (json/decode
-            (encode
-             (template
-              "sample"
-              :foo (parameter)
-              :bucket-policy
-              (s3/bucket-policy
-               {::s3/bucket (xref :foo)
-                ::iam/policy-document
-                #::iam{:statement
-                       [#::iam{:action ["s3:GetObject"]
-                               :effect "Allow"
-                               :principal {"Service" "lambda.amazonaws.com"}
-                               :condition {"StringEquals" {"aws:SourceArn" (xref :foo)}}
-                               :resource (xref :foo)}]}}))))))))
+    (is (= {"Type" "AWS::S3::BucketPolicy",
+            "Properties"
+            {"Bucket" {"Ref" "Foo"},
+             "PolicyDocument"
+             {"Statement"
+              [{"Action" ["s3:GetObject"],
+                "Effect" "Allow",
+                "Principal" {"Service" "lambda.amazonaws.com"},
+                "Condition" {"StringEquals" {"aws:SourceArn" {"Ref" "Foo"}}},
+                "Resource" {"Ref" "Foo"}}]}}}
+           (rewrite-element-data
+            (s3/bucket-policy
+             {::s3/bucket (xref :foo)
+              ::iam/policy-document
+              {::iam/statement
+               [{::iam/action ["s3:GetObject"]
+                 ::iam/effect "Allow"
+                 ::iam/principal {"Service" "lambda.amazonaws.com"}
+                 ::iam/condition {"StringEquals" {"aws:SourceArn" (xref :foo)}}
+                 ::iam/resource (xref :foo)}]}}))))))
 
 (deftest s3-cors-test
   (testing "encode"
     (is (= (json/decode (slurp (io/resource "aws/s3/s3-cors.json")))
-           (json/decode
-            (encode
-             (template
-              "sample"
-              :bucket
-              (s3/bucket
-               #::s3 {:access-control "PublicReadWrite"
-                      :cors-configuration #::s3 {:cors-rules
-                                                 [#::s3 {:allowed-headers ["*"]
-                                                         :allowed-methods ["GET"]
-                                                         :allowed-origins ["*"]
-                                                         :exposed-headers ["Date"]
-                                                         :id "myCORSRuleId1"
-                                                         :max-age 3600}
-                                                  #::s3 {:allowed-headers ["x-amz-*"]
-                                                         :allowed-methods ["DELETE"]
-                                                         :allowed-origins ["http://www.example1.com"
-                                                                           "http://www.example2.com"]
-                                                         :exposed-headers ["Connection"
-                                                                           "Server"
-                                                                           "Date"]
-                                                         :id "myCORSRuleId2"
-                                                         :max-age 1800}]}}))))))))
+           (rewrite-element-data
+            (s3/bucket
+             {::s3/access-control "PublicReadWrite"
+              ::s3/cors-configuration {::s3/cors-rules
+                                       [{::s3/allowed-headers ["*"]
+                                         ::s3/allowed-methods ["GET"]
+                                         ::s3/allowed-origins ["*"]
+                                         ::s3/exposed-headers ["Date"]
+                                         ::s3/id "myCORSRuleId1"
+                                         ::s3/max-age 3600}
+                                        {::s3/allowed-headers ["x-amz-*"]
+                                         ::s3/allowed-methods ["DELETE"]
+                                         ::s3/allowed-origins ["http://www.example1.com"
+                                                               "http://www.example2.com"]
+                                         ::s3/exposed-headers ["Connection"
+                                                               "Server"
+                                                               "Date"]
+                                         ::s3/id "myCORSRuleId2"
+                                         ::s3/max-age 1800}]}}))))))
