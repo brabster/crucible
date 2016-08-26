4d0672f0c1c8414962ee4235986828fe341d0972
Paul Brabban <paul.brabban@gmail.com>
2016-08-26 07:33:06 +0100

    missed a namespace change

--- test/crucible/aws/lambda_test.clj
+++ test/crucible/aws/lambda_test.clj
@@ -20,17 +20,17 @@
                           "VpcConfig" {"SecurityGroupIds" [{"Ref" "Foo"}], "SubnetIds" {"Ref" "Foo"}}
                           "Code" {"S3Bucket" {"Ref" "Foo"}, "S3Key" {"Ref" "Foo"}}}}
            (rewrite-element-data
-            (lambda/function {::l/handler (xref :foo)
-                              ::l/function-name (xref :foo)
-                              ::l/description (xref :foo)
-                              ::l/memory-size 1024
-                              ::l/timeout 300
-                              ::l/runtime "java8"
-                              ::l/role (xref :foo)
-                              ::l/code {::l/s3-bucket (xref :foo)
-                                        ::l/s3-key (xref :foo)}
-                              ::l/vpc-config {::l/security-group-ids [(xref :foo)]
-                                              ::l/subnet-ids (xref :foo)}}))))))
+            (l/function {::l/handler (xref :foo)
+                         ::l/function-name (xref :foo)
+                         ::l/description (xref :foo)
+                         ::l/memory-size 1024
+                         ::l/timeout 300
+                         ::l/runtime "java8"
+                         ::l/role (xref :foo)
+                         ::l/code {::l/s3-bucket (xref :foo)
+                                   ::l/s3-key (xref :foo)}
+                         ::l/vpc-config {::l/security-group-ids [(xref :foo)]
+                                         ::l/subnet-ids (xref :foo)}}))))))
 
 (deftest permission-test
   (testing "encode"
@@ -40,7 +40,7 @@
                           "Principal" "s3.amazonaws.com"
                           "SourceAccount" { "Ref" "AWS::AccountId" }}}
            (rewrite-element-data
-            (lambda/permission {::l/action "lambda:InvokeFunction"
-                                ::l/function-name (xref :my-lambda-function :arn)
-                                ::l/principal "s3.amazonaws.com"
-                                ::l/source-account account-id}))))))
+            (l/permission {::l/action "lambda:InvokeFunction"
+                           ::l/function-name (xref :my-lambda-function :arn)
+                           ::l/principal "s3.amazonaws.com"
+                           ::l/source-account account-id}))))))
