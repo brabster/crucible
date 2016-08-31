229543fbef518f7e520161e8ea797b0ae5cd58da
garethdavies <gareth.davies@skybettingandgaming.com>
2016-08-31 14:00:44 +0100

    Make firehose delivery stream name optional.

--- test/crucible/aws/firehose_test.clj
+++ test/crucible/aws/firehose_test.clj
@@ -5,7 +5,7 @@
             [clojure.test :refer :all]))
 
 (deftest firehose-test
-  (testing "encode"
+  (testing "encode with delivery stream name"
     (is (= {"AWSTemplateFormatVersion" "2010-09-09",
             "Description" "sample",
             "Parameters" {"Foo" {"Type" "String"}},
@@ -32,4 +32,33 @@
                                              :size-in-mbs (xref :foo)}
                       :compression-format "UNCOMPRESSED"
                       :prefix  (xref :foo)
-                      :role-arn  (xref :foo)}}))))))))
+                      :role-arn  (xref :foo)}})))))))
+  (testing "encode without delivery stream name"
+    (is (= {"AWSTemplateFormatVersion" "2010-09-09",
+            "Description" "sample",
+            "Parameters" {"Foo" {"Type" "String"}},
+            "Resources" {"Firehose"
+                          {"Type" "AWS::KinesisFirehose::DeliveryStream",
+                                     "Properties" {"DeliveryStreamName" {"Ref" "Foo"},
+                                                   "S3DestinationConfiguration"
+                                                      {"BucketARN" {"Ref" "Foo"},
+                                                       "BufferingHints" {"IntervalInSeconds" 9,
+                                                                         "SizeInMBs" {"Ref" "Foo"}},
+                                                       "CompressionFormat" "UNCOMPRESSED",
+                                                       "Prefix" {"Ref" "Foo"},
+                                                       "RoleARN" {"Ref" "Foo"}}}}}}
+          (json/decode
+            (encode
+              (template
+                "sample"
+                :foo (parameter)
+                :firehose
+                (fh/firehose
+                  {::fh/delivery-stream-name (xref :foo)
+                   ::fh/s3-destination-configuration
+                                             #::fh{:bucket-arn (xref :foo)
+                                                   :buffering-hints #::fh{:interval-in-seconds 9
+                                                                          :size-in-mbs (xref :foo)}
+                                                   :compression-format "UNCOMPRESSED"
+                                                   :prefix  (xref :foo)
+                                                   :role-arn  (xref :foo)}}))))))))
