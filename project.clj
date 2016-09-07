789f443acb4f66f57888daefab7d6602091b4b3f
Paul Brabban <paul.brabban@gmail.com>
2016-09-07 09:11:34 +0100

    Version 0.11.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject crucible "0.11.0"
+(defproject crucible "0.11.1-SNAPSHOT"
   :description "AWS Cloudformation templates in Clojure"
   :url "http://github.com/brabster/crucible"
   :license {:name "Eclipse Public License"
