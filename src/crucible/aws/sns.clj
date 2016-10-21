39e8b591f73a13c028e46cb3a9b0ca662d1d142f
Paul Brabban <paul.brabban@gmail.com>
2016-10-21 12:39:47 +0100

    add sns topic policy

--- src/crucible/aws/sns.clj
+++ src/crucible/aws/sns.clj
@@ -1,6 +1,7 @@
 (ns crucible.aws.sns
   "Resources in AWS::SNS::*"
   (:require [crucible.resources :refer [spec-or-ref defresource]]
+            [crucible.aws.iam :as iam]
             [clojure.spec :as s]))
 
 (s/def ::display-name (spec-or-ref string?))
@@ -11,3 +12,10 @@
                              ::topic-name]))
 
 (defresource topic "AWS::SNS::Topic" ::topic)
+
+(s/def ::topics (s/* (spec-or-ref string?)))
+
+(s/def ::topic-policy (s/keys :req [::iam/policy-document
+                                    ::topics]))
+
+(defresource topic-policy "AWS::SNS::TopicPolicy" ::topic-policy)
