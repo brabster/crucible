3a6279cc8a61e3488cd24dd3125878ea44f2848d
Paul Brabban <paul.brabban@gmail.com>
2016-12-08 14:25:12 +0000

    Feature/better deployment (#83)
    * drop old release tasks
    
    * prep for improved deployment process

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
+
+STABLE_VERSION=${TRAVIS_TAG}
+SNAPSHOT_VERSION="${TRAVIS_BRANCH//[\/]/-}-SNAPSHOT"
+export PROJECT_VERSION=${STABLE_VERSION:-"${SNAPSHOT_VERSION}"}
+echo "Project version is \"$PROJECT_VERSION\""
 
 lein deploy
