098399ee61b325288916473efffc98a6bb0e0f0e
Paul Brabban <paul.brabban@gmail.com>
2017-02-04 09:54:33 +0000

    skip deploy for scheduled builds

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if [ "$TRAVIS_EVENT_TYPE" = "cron" ];
+then
+    echo "Skipping deploy for scheduled build"
+    exit 0
+fi
+
 STABLE_VERSION=${TRAVIS_TAG}
 SNAPSHOT_VERSION="${TRAVIS_BRANCH//[\/]/-}-SNAPSHOT"
 export PROJECT_VERSION=${STABLE_VERSION:-"${SNAPSHOT_VERSION}"}
