c0ad1f36c6749bf923087fa41cda6bb02229b8de
Paul Brabban <paul.brabban@gmail.com>
2017-02-22 18:21:35 +0000

    setup events rule

---
+++ test/crucible/aws/events_test.clj
@@ -0,0 +1,20 @@
+(ns crucible.aws.events-test
+  (:require [crucible.aws.events :as events]
+            [crucible.assertion :refer [resource=]]
+            [crucible.core :refer [parameter xref]]
+            [cheshire.core :as json]
+            [clojure.test :refer :all :as t]
+            [clojure.java.io :as io]))
+
+(deftest minimal-events-test
+  (testing "encode"
+    (is (=
+         (json/decode (slurp (io/resource "aws/events/simple-event.json")))
+         (crucible.encoding/rewrite-element-data
+          (events/rule {::events/description "EventRule"
+                        ::events/event-pattern {"detail-type"
+                                                [ "AWS API Call via CloudTrail" ]
+                                                "detail" {"userIdentity" {"type" ["Root"]}}}
+                        ::events/state "ENABLED"
+                        ::events/targets [{::events/arn (xref :my-sns-topic)
+                                           ::events/id "OpsTopic"}]}))))))
