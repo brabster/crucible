3381f0b8dfbfc61516af89663edd68b3d11136e9
Edward Bowler <edward.bowler@skybettingandgaming.com>
2017-02-28 16:55:34 +0000

    wip

--- src/crucible_tools.clj
+++ src/crucible_tools.clj
@@ -3,62 +3,82 @@
             [clojure.test :refer :all :as t]
             [clojure.java.io :as io]
             [crucible.resources :refer [spec-or-ref defresource] :as res]
-            [camel-snake-kebab.core :refer [->kebab-case]]))
+            [camel-snake-kebab.core :refer [->kebab-case]]
+            [clojure.spec :as s]
+            [clojure.string :as st]))
 
-
-(def aws-spec (json/decode (slurp "https://d3teyb21fexa9r.cloudfront.net/latest/CloudFormationResourceSpecification.json")))
-(def property-types (get aws-spec "PropertyTypes"))
-(def resource-types (get aws-spec "ResourceTypes"))
-(def sorted-resource-keys (-> resource-types keys sort))
-(def sorted-property-keys (-> property-types keys sort))
+(defn ->spec-name [resource-type resource-name] ;TODO deal with AccessLoggingPolicy having missing data sometimes
+  (let [[ns-part el-part] (st/split resource-type #"\.")
+        ns (str "crucible.generated." (st/replace ns-part #"::" "."))
+        el (str el-part (when (and (not-empty el-part) (not-empty resource-name)) "-") resource-name)
+        x (keyword ns el)] ;->kebab-case
+    ;; (prn '>> x)
+    x))
 
 (defn primitive-type->spec [p]
   (case p
-    "String" `(spec-or-ref string?)
-    "Number" `(spec-or-ref number?)
+    "String"  `(spec-or-ref string?)
+    "Number"  `(spec-or-ref number?)
     "Integer" `(spec-or-ref integer?)
     "Boolean" `(spec-or-ref boolean?)
     `(spec-or-ref string?)))
 
 (defn non-primitive-type->spec [r i]
-   `(spec-or-ref ~(keyword (str r "."  i))))
+  (let [x (->spec-name r i) #_ (keyword (str (name r) "." i))] ;; TODO deal with StepAdjustment missing name part
+    ;; (prn '--> x)
+    `(spec-or-ref ~x)))
 
 (defn complex-type->spec [res type coll-item-type]
   (case type
     "List" `(s/coll-of ~coll-item-type :kind vector?)
-    "Map" `(s/map-of string? ~coll-item-type)
-    `(spec-or-ref ~(keyword (str res "."  type)))))
+    "Map"  `(s/map-of string? ~coll-item-type)
+    nil))
 
 (defn extract-spec-type [res prim-type type item-type coll-item-type]
   (if prim-type
     (primitive-type->spec prim-type)
     (complex-type->spec res type coll-item-type)))
 
-(defn ->spec [res x {:keys [Documentation PrimitiveType Required
-                         UpdateType DuplicatesAllowed PrimitiveItemType Type ItemType] :as d}]
-  (let [spec-name (keyword (name res) (name (->kebab-case x)))
-        coll-item-type (if PrimitiveItemType (primitive-type->spec PrimitiveItemType) (non-primitive-type->spec res ItemType))
-        spec-type (extract-spec-type res PrimitiveType Type ItemType coll-item-type) ]
-       {:spec `(s/def ~spec-name ~spec-type) :required Required :type (if PrimitiveType PrimitiveType Type)}))
+(defn ->spec [res x {:strs [Documentation PrimitiveType Required UpdateType DuplicatesAllowed PrimitiveItemType Type ItemType] :as d}]
+  (let [spec-name      (->spec-name res x)
+        coll-item-type (if PrimitiveItemType
+                         (primitive-type->spec PrimitiveItemType)
+                         (non-primitive-type->spec res ItemType))
+        spec-type      (when coll-item-type
+                         (extract-spec-type res PrimitiveType Type ItemType coll-item-type))]
+    #_ {:spec     `(s/def ~spec-name ~spec-type)
+        :required Required
+        :type     (if PrimitiveType PrimitiveType Type)}
+    (when spec-type
+      `[(s/def ~spec-name ~spec-type)])))
 
 (defn get-type-properties [r props]
-  (let [keyz (keys props)
-        data (map #(->spec r %1 (get props %1)) keyz)]
-       (vec data)))
+  (mapcat #(->spec r %1 (get props %1)) (keys props)))
 
-(defn kewyord-type [type]
-  (keyword type))
+(defn ->spec-keys [n prefix ks]
+  (->> ks
+       (keep first)
+       (map #(keyword n (str prefix "-" %)))
+       (into [])))
 
-(defn extract-properties [p]
-  (let [properties (-> (get property-types p) clojure.walk/keywordize-keys :Properties )]
-       {:type (kewyord-type p) :properties (get-type-properties p properties)}))
+(defn extract-properties [p properties]
+  (let [{required true optional false} (group-by #(get (val %) "Required") properties)
+        n (->spec-name p nil)]
+    ;; (prn '------>>> required optional properties)
+    `(s/def ~n (s/keys :req ~(->spec-keys (namespace n) (name n) required) :opt ~(->spec-keys (namespace n) (name n) optional)))))
 
-(defn extract-resources [r]
-  (let [resource (-> (get resource-types r) clojure.walk/keywordize-keys :Properties )]
-       {:type (kewyord-type r) :properties (get-type-properties r resource)}))
+(defn extract-resources [[p v]]
+  (let [properties (get v "Properties")]
+    (conj (get-type-properties p properties)
+          (extract-properties p properties))))
 
 (defn parse-resources []
-  {:property-types (vec (map extract-properties sorted-property-keys))
-  :resource-types (vec (map extract-resources sorted-resource-keys))})
+  (let [aws-spec (json/decode (slurp #_ "https://d3teyb21fexa9r.cloudfront.net/latest/CloudFormationResourceSpecification.json"
+                                     "test-resources/CloudFormationResourceSpecification.json"))
+        property-types (get aws-spec "PropertyTypes")
+        resource-types (get aws-spec "ResourceTypes")])
+  (->> (concat property-types resource-types)
+       (mapcat extract-resources)
+       (remove nil?)
+       (doall)))
 
-;(parse-resources)
