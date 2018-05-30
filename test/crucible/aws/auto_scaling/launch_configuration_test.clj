d7819ee732da825ce99815fcf5dbcae11665423f
Gert-Jan van de Streek <g.j.streek@avisi.nl>
2018-05-30 22:29:56 +0200

    Fix a broken test

--- test/crucible/aws/auto_scaling/launch_configuration_test.clj
+++ test/crucible/aws/auto_scaling/launch_configuration_test.clj
@@ -1,13 +1,15 @@
 (ns crucible.aws.auto-scaling.launch-configuration-test
   (:require [crucible.aws.auto-scaling.launch-configuration :as sut]
+            [crucible.core :refer [xref join] :as cf]
             [clojure.spec.alpha :as s]
             [clojure.test :refer :all]))
 
 (deftest launch-configuration-tests
   (testing "valid launch configuration"
     (is (s/valid? ::sut/launch-configuration
-                  {::sut/min-size "2"
-                   ::sut/max-size "2"})))
+                  {::sut/key-name "secret"
+                   ::sut/image-id "ami-something"
+                   ::sut/instance-type (xref :instance-type)})))
   (testing "invalid launch configuration"
     (is (not (s/valid? ::sut/launch-configuration
                        {})))))
