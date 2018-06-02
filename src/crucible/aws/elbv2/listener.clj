a3d541791182ccf467bcc2ca497b3ce0d51bec4b
Gert-Jan van de Streek <g.j.streek@avisi.nl>
2018-06-02 17:24:05 +0200

    Feature/elastic load balancer v2 (#138)
    * Add AWS::ElasticLoadBalancingV2::* support

---
+++ src/crucible/aws/elbv2/listener.clj
@@ -0,0 +1,25 @@
+(ns crucible.aws.elbv2.listener
+  "AWS::ElasticLoadBalancingV2::Listener"
+  (:require [clojure.spec.alpha :as s]
+            [crucible.resources :refer [spec-or-ref defresource] :as res]))
+
+;; http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-elasticloadbalancingv2-listener.html
+
+(s/def ::target-group-arn (spec-or-ref string?))
+(s/def ::type (spec-or-ref #{"forward"}))
+(s/def ::default-action (s/keys :req [::target-group-arn ::type]))
+(s/def ::default-actions (s/* ::default-action))
+(s/def ::load-balancer-arn (spec-or-ref string?))
+(s/def ::port (spec-or-ref integer?))
+(s/def ::protocol (spec-or-ref #{"HTTPS" "HTTP" "TCP"}))
+(s/def ::ssl-policy (spec-or-ref string?))
+(s/def ::certificate-arn (spec-or-ref string?))
+(s/def ::certificate (s/keys :opt [::certificate-arn]))
+(s/def ::certificates (s/* ::certificate))
+
+(s/def ::resource-spec (s/keys :req [::default-actions
+                                     ::load-balancer-arn
+                                     ::port
+                                     ::protocol]
+                               :opt [::ssl-policy
+                                     ::certificates]))
