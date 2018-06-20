8f48dcbe912a0ac8f3a2f86060e87163b47a2cec
Adam Frey <adam@adamfrey.me>
2018-06-20 12:51:52 -0400

    Add some missing attributes in kinesis and lambda (#148)
    Thanks @AdamFrey !
    
    * Add :crucible.aws.kinesis/retention-period-hours spec and optional key
    
    From AWS docs:
    https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-kinesis-stream.html
    
    * Add :crucible.aws.lambda/timeout to function spec
    
    It was in the namespace but never made it into the ::function spec

--- src/crucible/aws/kinesis.clj
+++ src/crucible/aws/kinesis.clj
@@ -4,11 +4,13 @@
             [crucible.resources :refer [spec-or-ref defresource]]))
 
 (s/def ::shard-count (spec-or-ref pos-int?))
+(s/def ::retention-period-hours (spec-or-ref pos-int?))
 
 (s/def ::name (spec-or-ref string?))
 
 (s/def ::stream (s/keys :req [::shard-count]
                         :opt [::name
+                              ::retention-period-hours
                               :crucible.resources/tags]))
 
 (defresource stream "AWS::Kinesis::Stream" ::stream)
