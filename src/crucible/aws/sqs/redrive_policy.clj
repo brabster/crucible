bcea1c46996dc6470374ef001f848af662f67fbc
Gert-Jan van de Streek <g.j.streek@avisi.nl>
2018-07-04 18:16:46 +0200

    Fix camel casing for redrive policy

--- src/crucible/aws/sqs/redrive_policy.clj
+++ src/crucible/aws/sqs/redrive_policy.clj
@@ -1,10 +1,14 @@
 (ns crucible.aws.sqs.redrive-policy
   "Amazon SQS RedrivePolicy, a property of AWS::SQS::Queue"
   (:require [clojure.spec.alpha :as s]
+            [crucible.encoding.keys :refer [->key]]
             [crucible.resources :refer [spec-or-ref defresource] :as res]))
 
 (s/def ::dead-letter-target-arn (spec-or-ref string?))
 (s/def ::max-receive-count (spec-or-ref integer?))
+
+(defmethod ->key :max-receive-count [_] "maxReceiveCount")
+(defmethod ->key :dead-letter-target-arn [_] "deadLetterTargetArn")
 
 (s/def ::resource-property-spec (s/keys :req [::dead-letter-target-arn
                                               ::max-receive-count]))
