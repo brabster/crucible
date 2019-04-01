544cb9a7f44f5ea97052c031d30969454e9a06c3
Sam Hewitt <shewitt@roomkey.com>
2019-04-01 11:00:50 -0400

    use appropriate types in cloudwatch

--- test/crucible/aws/cloudwatch_test.clj
+++ test/crucible/aws/cloudwatch_test.clj
@@ -14,9 +14,9 @@
                     ::cw/metric-name "CPUUtilization"
                     ::cw/namespace "AWS/EC2"
                     ::cw/statistic "Average"
-                    ::cw/period "300"
-                    ::cw/evaluation-periods "2"
-                    ::cw/threshold "90"
+                    ::cw/period 300
+                    ::cw/evaluation-periods 2
+                    ::cw/threshold 90.0
                     ::cw/alarm-actions [(xref :web-server-scale-up-policy)]
                     ::cw/dimensions [{::cw/name "AutoScalingGroupName"
                                       ::cw/value (xref :web-server-group)}]
