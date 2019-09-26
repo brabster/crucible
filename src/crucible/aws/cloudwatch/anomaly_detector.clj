c71bcb658d117bf66df3b03c92d41de065f29110
Cameron Sumpter <csumpter@roomkey.com>
2019-09-26 11:49:10 -0400

    Add support for cloudwatch anomaly detector

---
+++ src/crucible/aws/cloudwatch/anomaly_detector.clj
@@ -0,0 +1,46 @@
+(ns crucible.aws.cloudwatch.anomaly-detector
+  (:require [crucible.aws.cloudwatch :as cloudwatch]
+            [crucible.resources :refer [spec-or-ref defresource] :as res]
+            [crucible.encoding.keys :refer [->key]]
+            [crucible.values :as v]
+            [clojure.spec.alpha :as s]))
+
+(s/def ::value (spec-or-ref string?))
+
+(s/def ::name (spec-or-ref string?))
+
+(s/def ::dimension (s/keys :req [::name ::value]))
+
+(s/def ::dimensions (s/coll-of ::dimension :kind vector?))
+
+(s/def ::stat ::cloudwatch/statistic)
+
+(def date-time-regex
+  #"(19|20)[0-9][0-9]-(0[0-9]|1[0-2])-(0[1-9]|([12][0-9]|3[01]))T([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]")
+
+(s/def ::date-time (spec-or-ref (s/and string? #(re-matches date-time-regex %))))
+
+(s/def ::end-time ::date-time)
+
+(s/def ::start-time ::date-time)
+
+(s/def ::range (s/keys :req [::start-time ::end-time]))
+
+(s/def ::ranges (s/coll-of ::range))
+
+(s/def ::name-space (spec-or-ref string?))
+(defmethod ->key :name-space [_] "Namespace")
+
+(s/def ::metric-name (spec-or-ref string?))
+
+(s/def ::excluded-time-ranges (spec-or-ref ::ranges))
+
+(s/def ::metric-time-zone (spec-or-ref string?))
+
+(s/def ::configuration (s/keys :opt [::excluded-time-ranges ::metric-time-zone]))
+
+(s/def ::anomaly-detector (s/keys :req [::metric-name
+                                        ::name-space
+                                        ::stat]))
+
+(defresource anomaly-detector "AWS::CloudWatch::AnomalyDetector" ::anomaly-detector)
