b81f4346eae2657703addb7ddb8d166401059e55
Paul Brabban <paul.brabban@gmail.com>
2020-02-09 19:24:25 +0000

    Bump Cheshire

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[camel-snake-kebab "0.4.1"]
-                 [cheshire "5.9.0"]
+                 [cheshire "5.10.0"]
                  [org.clojure/tools.namespace "0.3.1"]
                  [org.clojure/tools.cli "0.4.2"]
                  [expound "0.8.4"]]
